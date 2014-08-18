9bbf01d03294549486497ba9679fbbd0bb956136
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2014-08-19 00:52:48 +0600

    replace text with string

--- lib/acts-as-taggable-array-on/taggable.rb
+++ lib/acts-as-taggable-array-on/taggable.rb
@@ -8,10 +8,10 @@ module ActsAsTaggableArrayOn
       def acts_as_taggable_array_on(*tag_def)
         tag_name = tag_def.first
 
-        scope :"with_any_#{tag_name}", ->(* tags){where("#{tag_name} && ARRAY[?]", tags)}
-        scope :"with_all_#{tag_name}", ->(* tags){where("#{tag_name} @> ARRAY[?]", tags)}
-        scope :"without_any_#{tag_name}", ->(* tags){where.not("#{tag_name} && ARRAY[?]", tags)}
-        scope :"without_all_#{tag_name}", ->(* tags){where.not("#{tag_name} @> ARRAY[?]", tags)}
+        scope :"with_any_#{tag_name}", ->(* tags){where("#{tag_name} && ARRAY[?]::varchar[]", tags)}
+        scope :"with_all_#{tag_name}", ->(* tags){where("#{tag_name} @> ARRAY[?]::varchar[]", tags)}
+        scope :"without_any_#{tag_name}", ->(* tags){where.not("#{tag_name} && ARRAY[?]::varchar[]", tags)}
+        scope :"without_all_#{tag_name}", ->(* tags){where.not("#{tag_name} @> ARRAY[?]::varchar[]", tags)}
 
         self.class.class_eval do
           define_method :"all_#{tag_name}" do |options = {}, &block|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ def create_database
   ActiveRecord::Schema.define(:version => 1) do
     create_table :users do |t|
       t.string :name
-      t.text :colors, array: true, defualt: '{}'
+      t.string :colors, array: true, defualt: '{}'
       t.timestamps
     end
   end
