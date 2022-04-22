42ab7d3c6581f4842324eb9b4443c66a820e940c
Christian Billen <cbillen@warmlyyours.com>
2022-04-21 21:26:58 -0500

    adding test

--- lib/acts-as-taggable-array-on/taggable.rb
+++ lib/acts-as-taggable-array-on/taggable.rb
@@ -22,15 +22,15 @@ module ActsAsTaggableArrayOn
           define_method :"all_#{tag_name}" do |options = {}, &block|
             subquery_scope = unscoped.select("unnest(#{table_name}.#{tag_name}) as tag").distinct
             subquery_scope = subquery_scope.instance_eval(&block) if block
-
-            unscoped(:where).from(subquery_scope).pluck("tag")
+            # Remove the STI inheritance type from the outer query since it is in the subquery
+            unscope(where: :type).from(subquery_scope).pluck(:tag)
           end
 
           define_method :"#{tag_name}_cloud" do |options = {}, &block|
             subquery_scope = unscoped.select("unnest(#{table_name}.#{tag_name}) as tag")
             subquery_scope = subquery_scope.instance_eval(&block) if block
-
-            unscoped(:where).from(subquery_scope).group("tag").order("tag").pluck(Arel.sql("tag, count(*) as count"))
+            # Remove the STI inheritance type from the outer query since it is in the subquery
+            unscope(where: :type).from(subquery_scope).group(:tag).order(:tag).count(:tag)
           end
         end
       end
